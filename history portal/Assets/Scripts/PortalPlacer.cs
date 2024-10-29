625be2b0ea037b58e4b508fd05e0a6e97f18c0fa
Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
2024-10-29 23:15:29 +0100

    Added highlight for the portal

--- history portal/Assets/Scripts/PortalPlacer.cs
+++ history portal/Assets/Scripts/PortalPlacer.cs
@@ -1,36 +1,69 @@
-using System.Collections;
 using System.Collections.Generic;
-using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
 public class PortalPlacer : PressInputBase
 {
     [SerializeField] public GameObject SpawnablePortal;
     [SerializeField] public ARRaycastManager raycastManager;
     [SerializeField] public ARPlaneManager planeManager;
+    [SerializeField] public GameObject markerPrefab;
 
     private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
-    private GameObject currentPortal; 
+    private GameObject currentPortal;
+    private GameObject markerInstance;
+    private Pose lastHitPose;
+
+    void Start()
+    {
+        if (markerPrefab != null)
+        {
+            markerInstance = Instantiate(markerPrefab);
+            markerInstance.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        UpdateMarkerPosition();
+    }
+
+    private void UpdateMarkerPosition()
+    {
+        Vector3 screenCenter = new Vector3(Screen.width / 2, Screen.height / 2);
+        if (raycastManager.Raycast(screenCenter, hits, TrackableType.PlaneWithinPolygon))
+        {
+            lastHitPose = hits[0].pose;
+
+            if (markerInstance != null)
+            {
+                markerInstance.SetActive(true);
+                markerInstance.transform.position = lastHitPose.position;
+                markerInstance.transform.rotation = Quaternion.Euler(-90, lastHitPose.rotation.eulerAngles.y, lastHitPose.rotation.eulerAngles.z);
+            }
+        }
+        else
+        {
+            if (markerInstance != null)
+            {
+                markerInstance.SetActive(false);
+            }
+        }
+    }
 
     protected override void OnPress(Vector3 position)
     {
-        if (!raycastManager.Raycast(position, hits, TrackableType.PlaneWithinPolygon)) return;
-
-        if (!isButtonPressed()) return;
-
-        var hitPose = hits[0].pose;
+        if (markerInstance == null || !markerInstance.activeSelf) return;
 
         if (currentPortal != null)
         {
             Destroy(currentPortal);
         }
 
-        currentPortal = Instantiate(SpawnablePortal, hitPose.position, hitPose.rotation);
+        currentPortal = Instantiate(SpawnablePortal, lastHitPose.position, lastHitPose.rotation);
     }
 
     public void SwitchLocation(GameObject location)
