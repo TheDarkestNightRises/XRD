df823d526ef6e021820c5c4ed955d22f3af0d3b1
Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
2024-11-09 00:55:12 +0100

    Optimized vr project with URP , Occlusion, Lights, VR specific settings

---
+++ super-hot/Assets/Scripts/EnemyBase.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+public abstract class EnemyBase : MonoBehaviour
+{
+    public bool isDead;
+    public GameObject hitParticlePrefab;
+    protected NavMeshAgent agent;
+    protected Animator animator;
+    public Transform playerTarget;
+    public UnityEvent onDeath;
+
+    public virtual void Start()
+    {
+        isDead = false;
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        SetUpRag();
+    }
+
+    public abstract void Update();
+
+    public void SetKinematic(bool kinematic)
+    {
+        foreach (var item in GetComponentsInChildren<Rigidbody>())
+        {
+            item.isKinematic = kinematic;
+        }
+    }
+
+    public void SetUpRag()
+    {
+        SetKinematic(true);
+    }
+
+    public virtual void Dead(Vector3 position)
+    {
+        if (isDead) return;
+        isDead = true;
+        SetKinematic(false);
+        animator.enabled = false;
+        agent.enabled = false;
+        this.enabled = false;
+        Debug.Log("I died");
+        onDeath.Invoke();
+    }
+}
