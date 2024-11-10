ef05c75f26e44bb510c33aa965bba48e9c3ddd8e
Oriana11 <rubyjpy@gmail.com>
2024-11-10 02:16:20 +0100

    Added scripts for restart and quit game

---
+++ super-not/Assets/Scripts/MenuScript/PauseMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject wristUI;
+
+    public bool activeWristUI = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisplayWristUI();
+    }
+
+    public void PauseButtonPressed(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            DisplayWristUI();
+        }
+    }
+
+    public void DisplayWristUI()
+    {
+        if(activeWristUI == true)
+        {
+            wristUI.SetActive(false);
+            activeWristUI = false;
+            Time.timeScale = 1;
+        }
+        else if(!activeWristUI)
+        {
+            wristUI.SetActive(true);
+            activeWristUI = true;
+            Time.timeScale = 0;
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
