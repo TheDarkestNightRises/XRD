90c567c9d4deb6aaf89485d1f63a9cc691196e96
Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
2024-11-25 11:33:28 +0100

    Added feedback haptic

---
+++ super-not/Assets/Scripts/Weapons/HapticFeedback.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HapticFeedback : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float intensity;
+    public float duration;
+
+    private void Start()
+    {
+        XRBaseInteractable interactable = GetComponent<XRBaseInteractable>();
+        interactable.activated.AddListener();
+    }
+
+    public void TriggerHaptic(BaseInteractionEventArgs eventArgs)
+    {
+        if (eventArgs.interactorObject is XRBaseControllerInteractor controllerInteractor)
+        {
+            TriggerHaptic(controllerInteractor.xrController);
+        }
+    }
+
+    public void TriggerHaptic(XRBaseController controller)
+    {
+        if (intensity > 0) controller.SendHapticImpulse(intensity, duration);
+    }
+}
